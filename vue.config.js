644f99ce47b0d6ef23400cd1d7ad7de32fd653dc
Is2lucy1 <lchung@unomaha.edu>
2021-12-04 21:24:26 -0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  pwa: {
-      workboxOptions: {
-        exclude: [/_redirects/]
-      }
-    }
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-project/'
+    : '/'
 }
